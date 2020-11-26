0d0ef9dca8584b590a8bf67b4438ffcf55c39051
rhetesi <rhetesi@gmail.com>
2020-11-26 19:43:37 +0100

    Add: requirements texts

--- assets/calculator.js
+++ assets/calculator.js
@@ -1,2 +1,29 @@
 'use strict';
 
+/*
+Kell:
+Eseményfigyelő a normál és a műveleti gombokhoz
+- lenyomásukra értékük megjelenítése a kijelző mezőjében
+Eseményfigyelő a 'C' gombra
+- lenyomására az kijelző input field tartalmának törlése
+Eseményfigyelő az 'egyenlő' gombra
+- lenyomására a kijelző input field kiolvasása
+-- a kiolvasott string szétszedése 'numbers' és 'operationSigns' tömbökre
+-- két tömb összehasonlítása, ha az 'oS' nagyobb vagy egynelő a 'n'-el, akkor 'ERROR'
+--- és ezen szöveg megjelenítése a kijelzőn
+-- Ha nem hiba, akkor bejárjuk a tömböket (map?):
+numbers tömb 'i' és i + 1' elemét változókba másoljuk?
+oS tömb 'i' eleme alapjám meghívjuk a műveleti függvényt
+az elvégzett művelet eredménye lesz az 'i' reduce hatására
+(valójában nem 'i', hanem 'result' tömb de most könnyebb így leírni itt)
+Ha elfogytak a tömbök elemei, akkor a  'result' tömb értékét kiírjuk a kijelzőre:
+-- appendAdjecantHTML
+
+Függvények
+sum(mmation) = a + b
+sub(traction) = a - b
+mul(tiply) = a * b
+div(ision) = a / b
+
+*/
+
